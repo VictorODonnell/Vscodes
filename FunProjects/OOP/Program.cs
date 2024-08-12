08704e62183f1e3b66d640343b8620115041a6e6
victor Odonnell <39573414+vipereaper@users.noreply.github.com>
2024-08-12 10:02:56 -0700

    vscodes

---
+++ FunProjects/OOP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Common;
+
+namespace oop 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int roll1 = 0;
+                int roll2 = 0;
+
+                Random numGen = new Random();
+                Random numGen2 = new Random();
+
+                Console.WriteLine("Press any key to roll");
+                Console.ReadKey();
+
+                roll1 = numGen.Next(1, 7);
+                roll2 = numGen.Next(1, 7);
+
+                System.Console.WriteLine("\nYour roll: " + roll1);
+                System.Threading.Thread.Sleep(1000);
+                System.Console.WriteLine("Enemy ai roll: " + roll2);
+
+                if(roll1 < roll2) {
+                    Console.WriteLine("You Lose");
+                }
+                else if (roll1 > roll2) {
+                    System.Console.WriteLine("You Win");
+                }
+                else {
+                    Console.WriteLine("Draw");
+                } 
+                Console.ReadKey();
+            }            
+        }        
+    }
+}
